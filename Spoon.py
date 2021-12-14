546947f24e86b5ddc73b8515b72f31eb97fd572c
kry-p <jhjung.dev@gmail.com>
2021-12-14 16:59:21 +0900

    Add QResources for app icon

--- Spoon.py
+++ Spoon.py
@@ -4,14 +4,16 @@ import sys
 from PySide6.QtWidgets import QApplication
 from PySide6.QtCore import QCoreApplication, Qt
 from PySide6.QtGui import QIcon
+from PySide6.QtUiTools import QUiLoader
 
 from ui.window import Ui_MainWindow
+import images
 
 
 if __name__ == "__main__":
     QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
     app = QApplication([])
-    app.setWindowIcon(QIcon('resources/cookie.ico'))
+    app.setWindowIcon(QIcon(':/resources/cookie.ico'))
     window = Ui_MainWindow()
     window.show()
     sys.exit(app.exec())
